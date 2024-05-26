8315f4c3727fd5408dba8e18cd15aeed45f23fe6
JIHYE <t01087633799@gmail.com>
2024-05-26 17:14:08 +0900

    remove max token limit

--- pages/api/wonyoung_answer.js
+++ pages/api/wonyoung_answer.js
@@ -7,13 +7,14 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 async function generateMinYongResponse(question, conversationHistory) {
+    
     const conversationMessages = conversationHistory.map((msg) => ({
         role: msg.type === 'send' ? 'user' : 'assistant',
         content: msg.text
     }));
 
     const messages = [
-        { role: "system", content: "너는 밝고 긍정적이며 배려심 깊은 답변으로 유명한 '원영'이라는 캐릭터야. 다음 질문에 원영 스타일로 대답해줘. 답변을 65자 이내로 반말로 해줘. '너' 대신 '웅니'를 사용해." },
+        { role: "system", content: "너는 밝고 긍정적이며 배려심 깊은 답변으로 유명한 '원영'이라는 캐릭터야. 다음 질문에 원영 스타일로 대답해줘. 답변을 반말로 해줘. '너' 대신 '웅니'를 30% 확률로 사용해. 해결책을 제시할 때는 그와 반대되는 상황을 제시하는 말투와 함께 '럭키비키잖앙☘️'을 30% 확률로 사용해." },
         ...conversationMessages,
         { role: "user", content: question },
     ];
@@ -22,7 +23,7 @@ async function generateMinYongResponse(question, conversationHistory) {
     const completion = await openai.createChatCompletion({
         model: "gpt-4o-2024-05-13",
         messages: messages,
-        max_tokens: 60,
+        //max_tokens: 60,
     });
 
     return completion.data.choices[0].message.content;
